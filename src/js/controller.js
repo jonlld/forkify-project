846563b13a2068b6b149c7c0ba1453db06ed3274
jonlld <87318928+jonlld@users.noreply.github.com>
2022-04-20 14:38:33 +0900

    remove deployment check

--- src/js/controller.js
+++ src/js/controller.js
@@ -131,10 +131,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application');
-};
-
 // Publisher-subscriber pattern - pass in the function so view can call it
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -144,7 +140,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 // debugger;
 init();
